7328792d3283e6500b2cd24d2fe40039fa1f9f79
traintrain115 <traintrain115@gmail.com>
2017-07-26 19:36:52 -0700

    Update bootchef.sh

--- bootchef.sh
+++ bootchef.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
 sudo sed -i s#Defaults.*requiretty#Defaults\ \!requiretty#g /etc/sudoers
+sudo yum -y makecache fast
+sudo yum -y install wget
 cd /tmp/
 echo "Downloading the rpm"
-sudo curl -o chef-12.19.36-1.el7.x86_64.rpm   http://10.0.0.119/chef-12.19.36-1.el7.x86_64.rpm
+wget  http://52.43.11.64/chef-13.2.20-1.el6.x86_64.rpm
 
 echo "Installating the rpm"
 
@@ -13,8 +14,8 @@ sudo mkdir -p /etc/chef
 sudo chmod -R 777 /etc/chef
 cd /etc/chef
 
-sudo curl -o /etc/chef/client.rb  http://10.0.0.119//client.rb
-#wget http://10.0.0.119/knife.rb
-sudo curl -o /etc/chef/sandykarhty20.pem  http://10.0.0.119/sandykarhty20.pem
+sudo curl -o /etc/chef/client.rb  http://52.43.11.64//client.rb
+#wget http://52.43.11.64/knife.rb
+sudo curl -o /etc/chef/sandykarhty20.pem  http://52.43.11.64/sandykarhty20.pem
 sudo chmod 400 /etc/chef/sandykarhty20.pem
 sudo chef-client
